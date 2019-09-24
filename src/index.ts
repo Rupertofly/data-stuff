fed344aa1968629e5614044fdcea1335acfeee70
Ruby Quail <rupertmoore@ruby-imac.local>
2019-09-24 20:34:19 +1000

    queerosc
    Signed-off-by: Ruby Quail <rupertmoore@ruby-imac.local>

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,13 @@
-import { select, range, interpolateCubehelixDefault } from 'd3';
-import { render } from 'react-dom';
-import { createElement } from 'react';
+import {
+  select,
+  range,
+  interpolateCubehelixDefault,
+  interpolateInferno,
+} from 'd3';
+import p5 from 'p5';
+import sketch from './sketch';
 
 const TAU = 2 * Math.PI;
-const $ = (ca: string) => document.querySelector(ca)!;
-(() => {
-  const r = (time: number) => {
-    render(
-      createElement(
-        'div',
-        {},
-        createElement('button', {}, (time / 1000).toFixed(2)),
-        createElement('p', {}, String(32))
-      ),
-      $('body')
-    );
-
-    requestAnimationFrame(r);
-  };
-  requestAnimationFrame(r);
-})();
-(() => {
-  let count = 0;
-  let cv: HTMLElement = document.getElementById('base-div')!;
-  const ps = cv.appendChild(document.createElement('p'));
-  const btn = cv.appendChild(document.createElement('button'));
-  btn.addEventListener('click', e => {
-    ps.innerText = String(count++);
-  });
-  const canvas = select('body')
-    .append('canvas')
-    .attr('width', 200)
-    .attr('height', 200)
-    .node()!
-    .getContext('2d')!;
-  for (const x of range(200)) {
-    canvas.beginPath();
-    canvas.moveTo(x, 0);
-    canvas.strokeStyle = interpolateCubehelixDefault(x / 200);
-    canvas.lineTo(x, 200);
-    canvas.stroke();
-  }
-})();
+const instance = new p5(sketch, document.querySelector(
+  '#base-div'
+)! as HTMLElement);
