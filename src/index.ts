5e29fd601f32be627af6228aca94f6ec0bb607ee
Ruby Quail <rupertmoore@ruby-imac.local>
2019-10-06 21:56:16 +1100

    now we're getting somewhere
    Signed-off-by: Ruby Quail <rupertmoore@ruby-imac.local>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,108 @@
 import {
   select,
   range,
-  interpolateCubehelixDefault,
-  interpolateInferno,
+  rgb,
+  interpolateCubehelixLong,
+  hcl,
+  interpolate,
+  easeCircleInOut,
 } from 'd3';
-import p5 from 'p5';
-import sketch from './sketch';
-const TAU = 2 * Math.PI;
-const instance = new p5(sketch, document.querySelector(
-  '#base-div'
-)! as HTMLElement);
+import { arrayStart, Dir, MouldParticle, sense, move } from './mouldUtils';
+
+// import p5 from 'p5';
+// import sketch from './backupSketch';
+// const TAU = 2 * Math.PI;
+// const instance = new p5(sketch, document.querySelector(
+//   '#base-div'
+// )! as HTMLElement);
+const { random: rand, floor: flr } = Math;
+const [width, height] = [512, 512];
+const drawingCanvas = select('body')
+  .append('canvas')
+  .style('width', width)
+  .style('height', height)
+  .attr('width', width)
+  .attr('height', height);
+const capturer = new CCapture({
+  format: 'webm',
+  name: 'mould' + new Date().toISOString(),
+  framerate: 60,
+  timeLimit: 30,
+  verbose: true,
+  // motionBlurFrames: 1,
+  quality: 100,
+});
+
+const gfx = drawingCanvas.node()!.getContext('2d')!;
+gfx.fillStyle = 'black';
+const startingData = gfx.getImageData(0, 0, width, height);
+gfx.fillRect(0, 0, width, height);
+for (let x of range(width))
+  for (let y of range(height)) {
+    const pos = arrayStart(x, y, width);
+    const value = flr(easeCircleInOut(rand()) * 150);
+    startingData.data.set([value, value, value, 255], pos);
+  }
+gfx.putImageData(startingData, 0, 0);
+
+const startingParticles = range(0.08 * width * height).map(i => {
+  const [x, y] = [flr(width * rand()), flr(height * rand())];
+  const d = flr(rand() * 16) as Dir;
+  return new MouldParticle(x, y, d);
+});
+console.log(startingParticles);
+const hcl2 = hcl('#00f494');
+const hcl1 = hcl('#2f0a4a');
+const hLerp = interpolate(hcl1.h, hcl2.h);
+const cLerp = interpolate(hcl1.c, hcl2.c);
+const lLerp = interpolate(hcl1.l, hcl2.l);
+const hclLerp = (t: number) => {
+  const h = hLerp(t);
+  const c = cLerp(t);
+  const l = lLerp(t);
+  return hcl(h, l, c);
+};
+capturer.start();
+capturer.capture(drawingCanvas.node()!);
+const renderLoop = (t: number, data: ImageData, pcls: MouldParticle[]) => {
+  const senseArr = pcls.map(p => {
+    return sense(p, data.data, data.width, data.height, 16);
+  });
+  const newPos = senseArr.map(s => {
+    if (s.move) {
+      const movedPcl = move(s.particle, data.width, data.height, 2);
+      const { x: nX, y: nY } = movedPcl;
+      data.data.set(
+        [255, 255, 255, 255],
+        arrayStart(movedPcl.x, movedPcl.y, data.width)
+      );
+      return movedPcl;
+    } else {
+      return new MouldParticle(s.particle.x, s.particle.y, s.newDirection);
+    }
+  });
+  for (let x of range(width))
+    for (let y of range(height)) {
+      let avg = 0;
+      for (let dx = -1; dx <= 1; dx++)
+        for (let dy = -1; dy <= 1; dy++) {
+          let nx = (width + x + dx) % width;
+          let ny = (height + y + dy) % height;
+          avg += data.data[arrayStart(nx, ny, width)] * 0.105;
+        }
+      avg = flr(avg);
+      data.data.set([avg, avg, avg, 255], arrayStart(x, y, width));
+    }
+  gfx.putImageData(data, 0, 0);
+  let newData = gfx.getImageData(0, 0, width, height);
+  const itch = interpolateCubehelixLong('#2f0a4a', '#00f494');
+  for (let i = 0; i < newData.data.length; i = i + 4) {
+    const v = newData.data[i] / 255;
+    const col = rgb(hclLerp(easeCircleInOut(v)));
+    newData.data.set([col.r, col.g, col.b, 255], i);
+  }
+  gfx.putImageData(newData, 0, 0);
+  capturer.capture(drawingCanvas.node()!);
+  requestAnimationFrame(() => renderLoop(0, data, newPos));
+};
+requestAnimationFrame(() => renderLoop(0, startingData, startingParticles));
--- src/mouldUtils.ts
+++ src/mouldUtils.ts
@@ -1,13 +1,28 @@
-enum Dir {
+export enum Dir {
   E,
+  ENE,
+  NE,
+  NNE,
   N,
+  NNW,
+  NW,
+  WNW,
   W,
+  WSW,
+  SW,
+  SSW,
   S,
+  SSE,
+  SE,
+  ESE,
 }
+const TAU = Math.PI * 2;
+const { sin, cos, floor: flr } = Math;
+export const degOfMv = 16;
 export const turns: (d: Dir) => { left: Dir; right: Dir } = d => {
   return {
-    left: (d + 1) % 4,
-    right: (4 + d - 1) % 4,
+    left: (d + 1) % degOfMv,
+    right: (degOfMv + d - 1) % degOfMv,
   };
 };
 export class MouldParticle {
@@ -20,7 +35,12 @@ export class MouldParticle {
     this.facing = n;
   }
 }
-export declare function getNewPosition(
+const _qc: (...args: number[]) => [number, number] = (x, y, n, d) => {
+  const deltaA = TAU / degOfMv;
+  const angle = n * deltaA;
+  return [flr(x + cos(angle) * d), flr(y + sin(angle) * d)];
+};
+export function getNewPosition(
   xp: number,
   yp: number,
   np: Dir,
@@ -28,24 +48,105 @@ export declare function getNewPosition(
   height: number,
   distance: number
 ): { x: number; y: number; facing: Dir };
-export declare function getNewPosition(
+export function getNewPosition(
   mp: MouldParticle,
   width: number,
   height: number,
   distance: number
 ): MouldParticle;
-export function getNewPosition(...args: (number | MouldParticle)[]) {
+export function getNewPosition(
+  ...args: (number | MouldParticle)[]
+): { x: number; y: number; facing: Dir } | MouldParticle {
   let x: number;
   let y: number;
   let n: Dir;
+  let wid: number;
+  let hei: number;
+  let stepLen: number;
   if (!(args[0] instanceof MouldParticle)) {
     x = args[0];
     y = args[1] as number;
     n = args[2] as Dir;
+    wid = args[3] as number;
+    hei = args[4] as number;
+    stepLen = args[5] as number;
   } else {
     let ap = args[0] as MouldParticle;
     x = ap.x;
     y = ap.y;
     n = ap.facing;
+    wid = args[1] as number;
+    hei = args[2] as number;
+    stepLen = args[3] as number;
   }
+  let [nX, nY] = _qc(x, y, n, stepLen);
+  nX = (wid + nX) % wid;
+  nY = (hei + nY) % hei;
+  return args[0] instanceof MouldParticle
+    ? new MouldParticle(nX, nY, n)
+    : { x: nX, y: nY, facing: n };
+}
+export function arrayStart(x: number, y: number, rad: number) {
+  return 4 * (x + y * rad);
+}
+export function sense(
+  pcl: MouldParticle,
+  source: ArrayLike<number>,
+  width: number,
+  height: number,
+  length: number
+) {
+  const currentDirection = pcl.facing;
+  const { left, right } = turns(currentDirection);
+  const leftSensorLocation = getNewPosition(
+    pcl.x,
+    pcl.y,
+    left,
+    width,
+    height,
+    length
+  );
+  const rightSensorLocation = getNewPosition(
+    pcl.x,
+    pcl.y,
+    right,
+    width,
+    height,
+    length
+  );
+  const forwardSensorLocation = getNewPosition(pcl, width, height, length);
+  let [lVal, fVal, rVal] = [
+    leftSensorLocation,
+    forwardSensorLocation,
+    rightSensorLocation,
+  ].map(p => {
+    return source[arrayStart(p.x, p.y, width)];
+  });
+  [lVal, fVal, rVal] = [lVal, fVal, rVal].map(n => (n > 200 ? 0 : n));
+  let move = true;
+  let nDirection = currentDirection;
+  if (fVal > lVal && fVal > rVal) move = true;
+  else if (fVal < lVal && fVal < rVal) {
+    move = false;
+    nDirection = Math.random() < 0.5 ? left : right;
+  } else if (lVal < rVal) {
+    move = false;
+    nDirection = right;
+  } else if (rVal < lVal) {
+    move = false;
+    nDirection = left;
+  }
+
+  return { particle: pcl, move, newDirection: nDirection };
+}
+export function move(
+  pcl: MouldParticle,
+  width: number,
+  height: number,
+  distance: number
+) {
+  return getNewPosition(pcl, width, height, distance);
+}
+export function turn(pcl: MouldParticle, direction: Dir) {
+  return new MouldParticle(pcl.x, pcl.y, direction);
 }
